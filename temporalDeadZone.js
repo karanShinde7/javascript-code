ceec83aa8e797102c3c05279fcb6d4132c97cb4e
karanShinde7 <karan.shinde27@gmail.com>
2024-07-14 19:51:25 +0530

    Temporal dead zone & Error types

---
+++ temporalDeadZone.js
@@ -0,0 +1,11 @@
+
+console.log("b ",b);
+console.log("a ", a); // Reference Error coz a is temporal dead zone
+let a = 10;
+var b = 10;
+
+// const c; // Syntax Error
+// let a = 20; // Syntax Error 
+
+const d = 102;
+// d = 120; // Type error
