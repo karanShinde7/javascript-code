244e2e5caa3017cfa24ef7234f268741da501ea6
karanShinde7 <karan.shinde27@gmail.com>
2024-07-14 16:13:54 +0530

    Function execution using scope

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ const getAge = () => {
 
 console.log("getAge", getAge);
 
+// Function execution using scope
+
+let temp = 1;
+a();
+b();
+console.log("temp value from global",temp);
+
+function a(){
+  let temp = 10;
+  console.log("temp value from a()",temp);
+}
+
+function b(){
+  let temp = 100;
+  console.log("temp value from b()",temp);
+}
