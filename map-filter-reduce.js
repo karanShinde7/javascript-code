03670f065e60cb7779b76e9c97ae7becf38852fa
karanShinde7 <karan.shinde27@gmail.com>
2024-07-19 16:22:34 +0530

    array reduce method

--- map-filter-reduce.js
+++ map-filter-reduce.js
@@ -1,36 +1,54 @@
-// Array map
+// // Array map
+// const arrMap = [5, 1, 6, 7, 2];
 
-const arrMap = [5,1,6,7,2];
+// const double = (val) => {
+//   return val * 2;
+// };
 
-const double = (val) => {
-    return val * 2;
-}
+// const triple = (val) => {
+//   return val * 3;
+// };
 
-const triple = (val) => {
-    return val * 3;
-}
+// const binary = (val) => {
+//   return val.toString(2);
+// };
 
-const binary = (val) => {
-    return val.toString(2);
-}
+// console.log("Original Map Array: ", arrMap);
+// console.log("Double: ", arrMap.map(double));
+// console.log("Triple: ", arrMap.map(triple));
+// console.log("Binary: ", arrMap.map(binary));
 
-console.log("Original Map Array: ", arrMap)
-console.log("Double: ", arrMap.map(double))
-console.log("Triple: ", arrMap.map(triple))
-console.log("Binary: ", arrMap.map(binary))
+// // Array filter
+// const arrFilter = [5, 1, 6, 7, 2];
 
+// const oddVal = (val) => {
+//   return val % 2;
+// };
 
-// Array filter
+// const evenVal = (val) => {
+//   return !(val % 2);
+// };
+// console.log("Original Filter array: ", arrFilter);
+// console.log("Double: ", arrFilter.filter(oddVal));
+// console.log("Triple: ", arrFilter.filter(evenVal));
 
-const arrFilter = [5,1,6,7,2];
+// Array reduce
+const arrReduce = [5, 1, 6, 7, 2];
 
-const oddVal = (val) => {
-    return val % 2;
-}
-
-const evenVal = (val) => {
-    return  !(val % 2);
-}
-console.log("Original Filter array: ", arrFilter)
-console.log("Double: ", arrFilter.filter(oddVal))
-console.log("Triple: ", arrFilter.filter(evenVal))
+console.log("Original Filter array: ", arrReduce);
+console.log(
+  "Addition: ",
+  arrReduce.reduce((acc, curr) => {
+    acc = acc + curr;
+    return acc;
+  }, 0)
+);
+console.log(
+  "Max no.: ",
+  arrReduce.reduce((acc, curr) => {
+    if (curr > acc ) {
+        acc = curr
+    } 
+    return acc;
+  }, 0)
+);
